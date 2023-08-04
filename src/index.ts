226dd4eb1fbbdad0a4fd658248dbbcfe0ee784aa
itzrizvi <shahriar.me.work@gmail.com>
2023-08-05 01:22:58 +0600

    All user auth routes initialized

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const port: number | string = process.env.PORT || 5000;
 
 // Import Routes
 import userRoutes from "./routes/userRoutes";
+import { errorHandler, notFound } from "./middlewares/errorHandler";
 // Create App
 const app = express();
 
@@ -12,6 +13,8 @@ const app = express();
 app.use("/api/users", userRoutes);
 // Root Route
 app.get("/", (req: Request, res: Response) => res.send("Server is Ready!!!"));
+// Error Handlers
+app.use([notFound, errorHandler]);
 // Server Listening
 app.listen(port, () => {
   console.log(`Server Is Started On PORT: ${port}`);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,16 @@
-import { authUser } from "../controllers/userController";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController";
 import express from "express";
 const router = express.Router();
 
 router.post("/auth", authUser);
+router.post("/", registerUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
