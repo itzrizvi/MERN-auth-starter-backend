aeec51869ec595fd0a639943c734966f8b083c01
itzrizvi <shahriar.me.work@gmail.com>
2023-08-04 23:43:13 +0600

    Error Handler Middlewares Configured

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 
+interface MongooseError extends Error {
+  kind?: string;
+}
+
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  const error: Error = new Error(`Not Found - ${req.originalUrl}`);
+  const error: MongooseError = new Error(`Not Found - ${req.originalUrl}`);
   res.sendStatus(404);
   next(error);
 };
+
+const errorHandler = (
+  error: MongooseError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = error.message;
+  // For Mongo DB Errors
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource Not Found";
+  }
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : error.stack,
+  });
+};
+
+export { notFound, errorHandler };
