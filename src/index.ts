d0a8fb6a47b1c3ea9d5b2730ce4600c723dc98c4
itzrizvi <shahriar.me.work@gmail.com>
2023-08-08 16:56:51 +0600

    Static File Connection Done and Ready For Deploy

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import connectDB from "./configs/db";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 const port: number | string = process.env.PORT || 5000;
 
@@ -19,8 +20,19 @@ app.use([
 
 // Route Assigning
 app.use("/api/users", userRoutes);
-// Root Route
-app.get("/", (req: Request, res: Response) => res.send("Server is Ready!!!"));
+// ENV CHECK and Connect to Static Files
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")),
+  );
+} else {
+  // Root Route
+  app.get("/", (req: Request, res: Response) => res.send("Server is Ready!!!"));
+}
+
 // Error Handlers
 app.use([notFound, errorHandler]);
 // Server Listening
